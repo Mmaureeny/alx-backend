7af1be7851cb88dafa35179d921cf88ec556ec82
Mmaureeny <muthonimaureen4@outlook.com>
2023-01-23 09:28:32 -0800

    task 0 & 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,9 +4,9 @@
 """
 from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Fetches the index range from a given page and page size.
     """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
